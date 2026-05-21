956eb53be202ffd6c401f2dd2e2a8088050ccce5
VictorSalinasMontanuy <victor.salinas.montanuy@estudiantat.upc.edu>
2026-05-21 13:30:45 +0200

    add key

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 // Reemplaza estas keys por 3 reales de vuestra base de datos de Render
 export const USERS = [
   { id: 1, name: "Usuari 1 (sergi)", apiKey: "TU_API_KEY_AQUI_1" },
-  { id: 2, name: "Usuari 2 (victor)", apiKey: "TU_API_KEY_AQUI_2" },
+  { id: 2, name: "Usuari 2 (victor)", apiKey: "a965d68e049cb45e49386312fa7a83b6" },
   { id: 3, name: "Usuari 3 (claudia)", apiKey: "TU_API_KEY_AQUI_3" },
   { id: 4, name: "Usuari 3 (Adria)", apiKey: "TU_API_KEY_AQUI_4" }
 ];
